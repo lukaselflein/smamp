374b41f86bd1caddaafb421efeabcc0ca42fb6bf
lukaselflein <elfleinl@tf.uni-freiburg.de>
2019-03-12 15:55:41 +0100

    fixed automatic version numbering

--- .update_version.py
+++ .update_version.py
@@ -8,9 +8,13 @@ outstring = ''
 with open('setup.py', 'r') as infile:
 	for line in infile:
 		if 'version' in line:
-			version = float(line[-7:-3])
+			split = line.split("'")
+			version = split[1]
+			version = float(version)		
 			version += 0.01
-			line = line[:-7] + str(version) + line[-3:]
+			version = '{:.2f}'.format(version)
+			line = split[0] + "'" + version + "'" + split[2]
+			print(line)
 		outstring += line
 
 with open('setup.py', 'w') as outfile:
