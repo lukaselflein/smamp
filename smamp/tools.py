77e78f0245736a8b7645aab7e8c33c28af425844
lukaselflein <elfleinl@cs.uni-freiburg.de>
2019-04-18 11:46:32 +0200

    auto update

--- smamp/tools.py
+++ smamp/tools.py
@@ -74,16 +74,17 @@ def find(path, folder_keyword, file_keyword, nr_occ=1, exclude_kw=['template', '
 		# Select the folders with the required keyword in them
 		if folder_keyword in subdir:
 
-			# Check if at least one desired file exists
-			if sum([file_keyword in f for f in files]) < 1:
-				print(os.getcwd())
-				raise RuntimeError('No {} file found.'.format(file_keyword))
+			if nr_occ is not None:
+				# Check if at least one desired file exists
+				if sum([file_keyword in f for f in files]) < 1:
+					print(os.getcwd())
+					raise RuntimeError('No {} file found.'.format(file_keyword))
 
-			# No more than one file must exists for uniqueness
-			if sum([file_keyword in f for f in files]) > nr_occ:
-				print(os.getcwd())
-				err = 'Multiple files containing {} found.'.format(file_keyword)
-				raise RuntimeError(err)
+				# No more than one file must exists for uniqueness
+				if sum([file_keyword in f for f in files]) > nr_occ:
+					print(os.getcwd())
+					err = 'Multiple files containing {} found.'.format(file_keyword)
+					raise RuntimeError(err)
 						
 			# If no error was raised, we can use the file we found:
 			for f in files:
@@ -106,3 +107,22 @@ def read_atom_numbers(path='../fitting_constraint_files/hydrogen_per_atom.csv'):
 	hydrogen_per_atom = df.to_dict()[df.columns[0]]
 
 	return hydrogen_per_atom
+
+
+def read_total_charge(path='../fitting_constraint_files/total_charge.csv'):
+	"""Determine total charge of molecule by reading file.
+	Arguments
+	path: Path to the table.
+
+	Returns:
+	charge: a float
+	"""
+	# The default charge is zero, use this if no file is found
+	if not os.path.isfile(path):
+		print('WARNING: No charge file found at {}. Using default charge of 0.0'.format(path))
+		return 0.0
+
+	with open(path) as charge_file:
+		for line in charge_file:
+			charge = float(line)
+	return charge
