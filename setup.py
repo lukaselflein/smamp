81a0f510368859670b884012c58c286264d50d43
lukaselflein <elfleinl@cs.uni-freiburg.de>
2019-04-18 12:23:50 +0200

    auto update

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='smamp',
-      version='1.04',
+      version='1.05',
       description='Self-consistently optimizing smamp charges.',
       packages=['smamp'],
       url='',
